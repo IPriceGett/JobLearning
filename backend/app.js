1bde035c3e05bb30a5bf7f8b06d006494700722b
Ignacio Price <=>
2023-11-16 13:08:26 -0300

    commit

--- backend/app.js
+++ backend/app.js
@@ -12,15 +12,6 @@ app.use(cors({
   origin: '*'
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
-
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
